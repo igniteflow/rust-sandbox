bbd47815341ac310b42209279587a8b11f1cd43c
Philip Tysoe <philtysoe@gmail.com>
2023-04-13 14:37:02 +0700

    Reverse a tuple

--- hello.rs
+++ hello.rs
@@ -2,4 +2,16 @@
 fn main() {
     let subject = "igniteflow";
     println!("Hello, {subject}!");
+
+    // tuple
+    let test_tuple = (1,2,3);
+    println!("{test_tuple:?}");
+    println!("{:?}", reverse(test_tuple));
+}
+
+fn reverse(easy_as: (i32, i32, i32)) -> (i32, i32, i32) {
+    // `let` can be used to bind the members of a tuple to variables.
+    let (x, y, z) = easy_as;
+
+    (z, y, x)
 }
