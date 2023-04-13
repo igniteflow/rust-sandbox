dfb1f0e966fcf6552815c56ae4ad63a2f92bb0f5
Philip Tysoe <philtysoe@gmail.com>
2023-04-13 14:37:03 +0700

    Array

--- hello.rs
+++ hello.rs
@@ -1,5 +1,6 @@
 // rustc hello.rs && ./hello
 fn main() {
+    // string
     let subject = "igniteflow";
     println!("Hello, {subject}!");
 
@@ -7,6 +8,10 @@ fn main() {
     let test_tuple = (1,2,3);
     println!("{test_tuple:?}");
     println!("{:?}", reverse(test_tuple));
+
+    // array
+    let test_array: [i32; 6] = [1, 2, 3, 4, 5, 6];
+    println!("{test_array:?}")
 }
 
 fn reverse(easy_as: (i32, i32, i32)) -> (i32, i32, i32) {
