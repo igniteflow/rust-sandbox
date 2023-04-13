edcfe6941f9266bf01e069c1613cff57a412e3e5
Philip Tysoe <philtysoe@gmail.com>
2023-04-13 14:37:03 +0700

    Print an emoji for Easter

---
+++ emoji.rs
@@ -0,0 +1,8 @@
+#![allow(unused)]
+
+fn main() {
+    // from https://symbl.cc/en/1F423/ > Encoding > dec (bytes)
+    let hatching_chick_bytes = vec![240, 159, 144, 163];
+    let hatching_chick = String::from_utf8(hatching_chick_bytes).unwrap();
+    println!("{hatching_chick}");
+}
