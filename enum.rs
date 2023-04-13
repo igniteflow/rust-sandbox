a34f10482585619dc7ccf15f3db5fb1087ed5161
Philip Tysoe <philtysoe@gmail.com>
2023-04-13 17:34:30 +0700

    Add "use" to enum example
    Syntactic sugar

--- enum.rs
+++ enum.rs
@@ -1,10 +1,10 @@
 /*
-    ❯ rustc enum.rs && ./enum
-    🔴
-    🟡
-    🟢
- */
-
+   ❯ rustc enum.rs && ./enum
+   🔴
+   🟡
+   🟢
+*/
+#![allow(dead_code)]
 enum TrafficLight {
     Red,
     Amber,
@@ -24,12 +24,26 @@ fn traffic_light_status(light: TrafficLight) {
     }
 }
 
+fn traffic_light_status_with_use(light: TrafficLight) {
+    let red = String::from_utf8(vec![240, 159, 148, 180]).unwrap();
+    let amber = String::from_utf8(vec![240, 159, 159, 161]).unwrap();
+    let green = String::from_utf8(vec![240, 159, 159, 162]).unwrap();
+
+    // The use declaration can be used to bind a full path to a new name, for easier access
+    use crate::TrafficLight::*;
+    match light {
+        Green => println!("{green}"),
+        Amber => println!("{amber}"),
+        Red => println!("{red}"),
+    }
+}
+
 fn main() {
     let green = TrafficLight::Green;
     let amber = TrafficLight::Amber;
     let red = TrafficLight::Red;
 
-    traffic_light_status(red);
-    traffic_light_status(amber);
-    traffic_light_status(green);
+    traffic_light_status_with_use(red);
+    traffic_light_status_with_use(amber);
+    traffic_light_status_with_use(green);
 }
