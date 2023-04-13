f209443fb89a8d42be1a49e1674b10074580f9b2
Philip Tysoe <philtysoe@gmail.com>
2023-04-13 14:37:03 +0700

    Reverse an array

--- hello.rs
+++ hello.rs
@@ -5,13 +5,14 @@ fn main() {
     println!("Hello, {subject}!");
 
     // tuple
-    let test_tuple = (1,2,3);
+    let test_tuple = (1, 2, 3);
     println!("{test_tuple:?}");
     println!("{:?}", reverse(test_tuple));
 
     // array
     let test_array: [i32; 6] = [1, 2, 3, 4, 5, 6];
-    println!("{test_array:?}")
+    println!("{test_array:?}");
+    println!("{:?}", reverse_array(test_array));
 }
 
 fn reverse(easy_as: (i32, i32, i32)) -> (i32, i32, i32) {
@@ -19,4 +20,10 @@ fn reverse(easy_as: (i32, i32, i32)) -> (i32, i32, i32) {
     let (x, y, z) = easy_as;
 
     (z, y, x)
+}
+
+fn reverse_array(easy_as: [i32; 6]) -> [i32; 6] {
+    let [a, b, c, x, y, z] = easy_as;
+
+    [z, y, x, c, b, a]
 }
