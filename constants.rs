1ff0e0249eaf7720fa1e916c0638f84adc39a384
Philip Tysoe <philtysoe@gmail.com>
2023-04-14 16:16:09 +0700

    Add constants (global and static)

---
+++ constants.rs
@@ -0,0 +1,8 @@
+const ABSOLUTE_ZERO_C: f32 = -273.15;
+static NOW_C: f32 = 25.0;
+
+
+fn main() {
+    println!("Absolute zero is {ABSOLUTE_ZERO_C}°C");
+    println!("It is {NOW_C}°C somewhere now, probably.");
+}
