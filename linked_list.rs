97f3cabb27167d6694dec27f3019d952d2ec04a9
Philip Tysoe <philtysoe@gmail.com>
2023-04-14 16:46:11 +0700

    Add simple linked list

---
+++ linked_list.rs
@@ -0,0 +1,51 @@
+/*
+    ❯ rustc linked_list.rs && ./linked_list
+    Length: 5
+    Linked List: 5, 4, 3, 2, 1, Nil
+ */
+
+use crate::List::*;
+
+enum List {
+    Cons(u32, Box<List>),
+    Nil,
+}
+
+impl List {
+    fn new() -> List {
+        Nil
+    }
+
+    fn prepend(self, elem: u32) -> List {
+        Cons(elem, Box::new(self))
+    }
+
+    fn len(&self) -> u32 {
+        match *self {
+            Cons(_, ref tail) => 1 + tail.len(),
+            Nil => 0
+        }
+    }
+
+    fn stringify(&self) -> String {
+        match *self {
+            Cons(head, ref tail) => {
+                format!("{}, {}", head, tail.stringify())
+            },
+            Nil => {
+                format!("Nil")
+            }
+        }
+    }
+}
+
+fn main() {
+    let mut list = List::new();
+    list = list.prepend(1);
+    list = list.prepend(2);
+    list = list.prepend(3);
+    list = list.prepend(4);
+    list = list.prepend(5);
+    println!("Length: {}", list.len());
+    println!("Linked List: {}", list.stringify());
+}
