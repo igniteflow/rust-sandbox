fc2ab14ca6b64e95340ed4abc1753f6a84fb6e68
Philip Tysoe <philtysoe@gmail.com>
2023-06-05 17:07:24 +0100

    Assign environment variable value to var

--- cargo_exec/src/main.rs
+++ cargo_exec/src/main.rs
@@ -6,12 +6,19 @@ fn main() {
         ❯ cargo build
         Compiling cargo_exec v0.1.0 (/Users/ptysoe/Projects/personal/rust-sandbox/cargo_exec)
             Finished dev [unoptimized + debuginfo] target(s) in 0.86s
-        ❯ ./target/debug/cargo_exec
-        EXAMPLE: "Hello, world"
+        ❯ export EXAMPLE="foobar" && ./target/debug/cargo_exec
+        EXAMPLE: "foobar"
+        Value: "foobar"
      */
     let key = "EXAMPLE";
+
+    // print the value if exists else print error
     match env::var(key) {
         Ok(val) => println!("{key}: {val:?}"),
         Err(e) => println!("Error reading environment var {key}: {e}"),
     }
+
+    // assign to var and then print - note does not handle error
+    let env_val = env::var(key).unwrap();
+    println!("Value: {env_val:?}");
 }
