1d9bd35aedd1f1675a287454857cf0465a305e6d
Philip Tysoe <philtysoe@gmail.com>
2023-06-05 12:08:25 +0100

    Read and print environment variable

--- cargo_exec/src/main.rs
+++ cargo_exec/src/main.rs
@@ -1,3 +1,17 @@
+use std::env;
+
 fn main() {
-    println!("Hello, world!");
+    /*
+        ❯ export EXAMPLE='Hello, world'
+        ❯ cargo build
+        Compiling cargo_exec v0.1.0 (/Users/ptysoe/Projects/personal/rust-sandbox/cargo_exec)
+            Finished dev [unoptimized + debuginfo] target(s) in 0.86s
+        ❯ ./target/debug/cargo_exec
+        EXAMPLE: "Hello, world"
+     */
+    let key = "EXAMPLE";
+    match env::var(key) {
+        Ok(val) => println!("{key}: {val:?}"),
+        Err(e) => println!("Error reading environment var {key}: {e}"),
+    }
 }
